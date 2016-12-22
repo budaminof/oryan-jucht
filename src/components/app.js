043bda89045827e7e759648d7b614402398b225d
bud aminof <budaminof@gmail.com>
2016-12-22 11:10:40 -0700

    second try

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { TranslatorProvider } from "react-translate";
+
+import EN from '../i18n/en';
+import HE from '../i18n/he';
 import Footer from '../components/footer';
 import Nav from '../components/nav';
 import About from '../components/about';
 
 export default class App extends Component {
+
   render() {
     return (
-      <main>
-        <Nav />
-          <div className="container-fluid">
-            { this.props.children }
-          </div>
-        <Footer />
-      </main>
+      <TranslatorProvider translations={EN}>
+        <main>
+          <Nav />
+            <div className="container-fluid">
+              { this.props.children }
+            </div>
+          <Footer />
+        </main>
+      </TranslatorProvider>
     );
   }
 }
--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,22 +1,23 @@
 export default {
-  nav: {
+  locale: 'en',
+  Nav: {
     name: 'Oryan Jucht',
     about: 'About',
     puna: 'Puna',
     classes: 'Classes'
   },
 
-  about: {
+  About: {
     header: 'About Oryan',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
 
-  puna: {
+  Puna: {
     header: 'Oryan in puna',
     main: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
 
-  classes: {
+  Classes: {
     header: 'Come join me in one of my classes',
     contact: 'Dont hesitate to contact me',
     email: 'exmaple1234@gmail.com'
